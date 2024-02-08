a83e36c15b657ab9ab33df9a0451f057c0a53899
George Papadimitriou <georgpap@isi.edu>
2024-02-08 00:40:09 -0800

    beehive cloud processing

---
+++ beehive-cloud-processing/executables/raingauge_totals.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+This example demonstrates querying rain gauge data and printing the total
+number of measurements grouped by VSN and sensor.
+"""
+import sage_data_client
+
+# query and load data into pandas data frame
+df = sage_data_client.query(
+    start="-1h",
+    filter={
+        "name": "env.raingauge.*",
+    }
+)
+
+# print number of results of each name
+#print(df.groupby(["meta.vsn", "name"]).size())
+print(df.groupby(["meta.vsn", "meta.sensor"]).value.agg(["size", "min", "max", "mean"]))
